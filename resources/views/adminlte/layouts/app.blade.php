c755a416da6773d6a6bb6091c5f30dcf3353345c
Boedy Irhadtanto <boedyirh@yahoo.co.id>
2024-07-14 05:27:43 +0700

    Fork

--- resources/views/adminlte/layouts/app.blade.php
+++ resources/views/adminlte/layouts/app.blade.php
@@ -196,7 +196,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Starter Pages
+                Starter Pages 1
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
